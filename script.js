7c12329738da2495c19daff89c638fea3ae10864
ArissaHoriye <aline_arissa@hotmail.com>
2024-09-30 11:25:31 -0300

    button, display

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function idButtonDay() {
         x.style.display = "none";
     }
 }
+
+function idPanel(){
+    var custDiv = document.getElementById("cust");
+    if (custDiv.style.display === "none"){
+        custDiv.style.display = "block";
+    }
+    else {
+        custDiv.style.display = "none"
+    }
+}
