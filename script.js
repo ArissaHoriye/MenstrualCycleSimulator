64d112ce84dc644e791f8ad6088377fa04bc9d14
ArissaHoriye <aline_arissa@hotmail.com>
2024-10-01 12:59:44 -0300

    modal

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function displayText1() {
     var x = document.getElementById("Aug28");
     if(x.style.display === "none"){
@@ -16,4 +17,47 @@ function displayText2() {
     else {
         x.style.display = "none";
     }   
+}
+*/
+
+/*var switchModal = () => {
+    var modal = document.querySelector(".modal")
+    var actualStyle = modal.style.display
+    if(actualStyle === "block") {
+        modal.style.display = "none"
+    }
+    else {
+        modal.style.display = "block"
+    }
+}
+
+var btn =
+document.querySelector(".modalBtn")
+btn.addEventListener("click",switchModal)
+
+window.onclick = function(event) {
+    var modal =
+    document.querySelector(".modal")
+    if(event.target == modal) {
+        switchModal()
+    }
+}
+*/
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
